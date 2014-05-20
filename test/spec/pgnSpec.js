98d0ffb5cc86ee7ac8f9c851101cb8e170c4be3d
Markus Liebelt <markusliebelt@gmail.com>
2014-05-20 20:47:37 +0200

    Added some test cases for check, mate, promotion.

--- test/spec/pgnSpec.js
+++ test/spec/pgnSpec.js
@@ -30,6 +30,75 @@ describe("When working with a pgn file as string", function() {
 
 })
 
+describe("When working with different PGN beginnings and endings", function() {
+    var my_pgn;
+
+    it("should work with no moves at all", function() {
+        my_pgn = pgnReader({pgn: ""});
+        expect(my_pgn.getMoves().length).toEqual(0);
+    });
+
+    it("should work with white's first move only", function() {
+        my_pgn = pgnReader({pgn: "1. e4"});
+        expect(my_pgn.getMoves().length).toEqual(1);
+        expect(my_pgn.getMoves()[0].notation).toEqual("e4");
+        expect(my_pgn.getMoves()[0].moveNumber).toEqual(1);
+    })
+
+    it ("should work with black's first move only", function() {
+        my_pgn = pgnReader({pgn: "1... e5"});
+        expect(my_pgn.getMoves().length).toEqual(1);
+        expect(my_pgn.getMoves()[0].notation).toEqual("e5");
+        expect(my_pgn.getMoves()[0].turn).toEqual("b");
+        expect(my_pgn.getMoves()[0].moveNumber).toEqual(1);
+    })
+
+    it ("should work with white beginning and black ending", function() {
+        my_pgn = pgnReader({pgn: "1. e4 e5 2. d4 cxd4"});
+        expect(my_pgn.getMoves().length).toEqual(4);
+        expect(my_pgn.getMoves()[0].notation).toEqual("e4");
+        expect(my_pgn.getMoves()[0].turn).toEqual("w");
+        expect(my_pgn.getMoves()[0].moveNumber).toEqual(1);
+        expect(my_pgn.getMoves()[3].notation).toEqual("cxd4");
+        expect(my_pgn.getMoves()[3].turn).toEqual("b");
+        expect(my_pgn.getMoves()[3].moveNumber).toBeUndefined();
+    })
+
+    it ("should work with black beginning and white ending", function() {
+        my_pgn = pgnReader({pgn: "1... e5 2. d4 cxd4 3. c3"});
+        expect(my_pgn.getMoves().length).toEqual(4);
+        expect(my_pgn.getMoves()[0].notation).toEqual("e5");
+        expect(my_pgn.getMoves()[0].turn).toEqual("b");
+        expect(my_pgn.getMoves()[0].moveNumber).toEqual(1);
+        expect(my_pgn.getMoves()[1].notation).toEqual("d4");
+        expect(my_pgn.getMoves()[1].turn).toEqual("w");
+        expect(my_pgn.getMoves()[1].moveNumber).toEqual(2);
+        expect(my_pgn.getMoves()[2].notation).toEqual("cxd4");
+        expect(my_pgn.getMoves()[2].turn).toEqual("b");
+        expect(my_pgn.getMoves()[2].moveNumber).toBeUndefined();
+    })
+})
+
+describe("When using all kind of notation", function() {
+    var my_pgn;
+    it ("should know how to move all kind of figures", function() {
+        my_pgn = pgnReader({pgn: "1. e4 Nf6 2. Bb5 c6 3. Ba4 Qa5 4. Nc3 Nf6 5. O-O e6 6. Re1 "});
+        expect(my_pgn.getMoves().length).toEqual(11);
+    })
+
+    it ("should know different variants of strikes", function() {
+        my_pgn = pgnReader({pgn: "1. e5 d5 2. xd5 Nc6 3. 5xc6 bxc6 4. Qdxd6"});
+        expect(my_pgn.getMoves().length).toEqual(7);
+        expect(my_pgn.getMoves()[2].notation).toEqual("xd5");
+    })
+
+    it ("should know all special symbols normally needed (promotion, check, mate)", function() {
+        my_pgn = pgnReader({pgn: "1. f3 e5 2. g4 Qh4#"});
+        expect(my_pgn.getMoves().length).toEqual(4);
+        my_pgn = pgnReader({pgn: "1. e7+ d2 2. e8=Q d1=R#"});
+    })
+})
+
 describe("When reading PGN with headers", function() {
     beforeEach(function() {
         var pgn_string = ['[Event "Casual Game"]',
