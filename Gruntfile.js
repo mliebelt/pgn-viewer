d3559cd3c0b871b7c980603336ae9c2cda1c12f1
Markus Liebelt <markusliebelt@gmail.com>
2014-06-20 14:52:30 +0200

    Added some Grunt tasks for copying files from chessboardjs, clean and markdown.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,52 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: ["dist/cs", 'dist/js', 'dist/img', 'dist/locales'],
         concat: {
-            dist: {
+            js: {
                 src: [
                     'chessboardjs/js/jquery-1.11.1.js',
                     'chess.js/chess.js',
                     'chessboardjs/js/chessboard.js',
                     'chessboardjs/js/json3.min.js',
                     'js/*.js'],
-                dest: 'dist/pgnviewerjs.js'
+                dest: 'dist/js/pgnviewerjs.js'
             }
         },
         uglify: {
-            dist: {
-                src: ['dist/pgnviewerjs.js'],
-                dest: 'dist/min/pgnviewerjs.js'
+            js: {
+                src: ['dist/js/pgnviewerjs.js'],
+                dest: 'dist/js/min/pgnviewerjs.js'
+            }
+        },
+        copy: {
+            all: {
+                files: [
+                    {
+                        src: ['locales/**', 'img/buttons/**', 'img/chesspieces/**', 'img/pattern/**', 'img/*.png', 'css/**'],
+                        dest: 'dist',
+                        expand: true
+                    }
+                ]
+            },
+            chessboardjs: {
+                files: [
+                    {
+                        expand: true, cwd: 'chessboardjs/', src: ['img/chesspieces/**', 'css/chessboard.css'],
+                        dest: 'dist/'}
+                ]
+            }
+        },
+        markdown: {
+            all: {
+                files: [
+                    {
+                        expand: true,
+                        src: '*.md',
+                        dest: 'dist/',
+                        ext: '.html'
+                    }
+                ]
             }
         }
     });
@@ -25,8 +56,11 @@ module.exports = function(grunt) {
     // Load the necessary tasks
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-markdown');
 
     // Default task.
-    grunt.registerTask('default', ['concat',  'uglify']);
+    grunt.registerTask('default', ['clean', 'concat',  'uglify', 'copy']);
 
 };
