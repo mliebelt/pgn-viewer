26c086fe7f9053cd25484aa3d528bb64dff0cf55
Markus Liebelt <markusliebelt@gmail.com>
2018-04-04 11:25:50 +0200

    #77 Allow redefinition of localPath (and others)

--- test/spec/PgnViewerJsSpec.js
+++ test/spec/PgnViewerJsSpec.js
@@ -19,7 +19,8 @@ describe("PGN Viewer", function() {
         beforeEach(function() {
             clearHTML("b");
             var pgn = "1. e4 e5 2. Nf3 Nc6  3.Bc4 Bc5 4.b4 Bxb4 5.c3 Ba5 6.d4 exd4 7.O-O d3 8.Qb3 Qf6 9.e5 Qg6 10.Re1 Nge7 11.Ba3 b5 12.Qxb5 Rb8 13.Qa4 Bb6 14.Nbd2 Bb7 15.Ne4 Qf5 16.Bxd3 Qh5 17.Nf6+ gxf6 18.exf6 Rg8 19.Rad1 Qxf3 20.Rxe7+ Nxe7 21.Qxd7+ Kxd7 22.Bf5+ Ke8 23.Bd7+ Kf8 24. Bxe7#";
-            pgnv = pgnView("b", {pgn: pgn, position: "start"});
+            pgnView("b", {pgn: pgn, position: "start"});
+            pgnv = window.pgnTestRegistry['b'];
         });
 
         it("should have 47 half-moves", function() {
@@ -39,7 +40,8 @@ describe("PGN Viewer", function() {
                 'Bb6 14.Nbd2 Bb7 15.Ne4 Qf5 16.Bxd3 Qh5 17.Nf6+ gxf6 18.exf6',
                 'Rg8 19.Rad1 Qxf3 20.Rxe7+ Nxe7 21.Qxd7+ Kxd7 22.Bf5+ Ke8',
                 '23.Bd7+ Kf8 24. Bxe7#'].join(" ");
-            pgnv = pgnView("b", {pgn: pgn, position: 'start'});
+            pgnView("b", {pgn: pgn, position: 'start'});
+            pgnv = window.pgnTestRegistry['b'];
         });
 
         it("should have these headers read", function() {
@@ -51,7 +53,8 @@ describe("PGN Viewer", function() {
         beforeEach(function() {
             clearHTML("b");
             var pgn = '1. e4 { first comment } e5 { second comment } 2. {now before } Nf3';
-            pgnv = pgnView("b", {pgn: pgn, position: 'start'});
+            pgnView("b", {pgn: pgn, position: 'start'});
+            pgnv = window.pgnTestRegistry['b'];
         });
 
         it ("should have seen these comments", function() {
@@ -71,8 +74,9 @@ describe("PGN Viewer", function() {
         });
 
         it("should understand comments for variation with white", function() {
-            var my_pgn = pgnView("b", {pgn: "1. d4 ({START} 1. {BEFORE} e4 {AFTER} e5) 1... d5"});
-            var var_first = my_pgn.getPgn().getMove(0).variations[0];
+            pgnView("b", {pgn: "1. d4 ({START} 1. {BEFORE} e4 {AFTER} e5) 1... d5"});
+            pgnv = window.pgnTestRegistry['b'];
+            var var_first = pgnv.getPgn().getMove(0).variations[0];
             expect(var_first.commentMove).toEqual("START");
             expect(var_first.commentBefore).toEqual("BEFORE");
             expect(var_first.commentAfter).toEqual("AFTER");
@@ -82,11 +86,12 @@ describe("PGN Viewer", function() {
     describe("When only displaying a board", function() {
         beforeEach(function() {
             clearHTML("b");
-            var pgnb = pgnBoard("b",
+            pgnBoard("b",
                 {position: 'start', pieceStyle: 'maya', theme: 'blue'});
+            pgnv = window.pgnTestRegistry['b'];
         });
 
-        it ("should have the start position", function() {
+        xit ("should have the start position", function() {
             //$('#bInner');
         })
     });
@@ -94,11 +99,11 @@ describe("PGN Viewer", function() {
     describe("When opening an editor", function() {
         beforeEach(function() {
             clearHTML("b");
-            var pgne = pgnEdit("b",
+            pgnEdit("b",
                 {position: 'start'});
         });
 
-        it ("should have the start position", function() {
+        xit ("should have the start position", function() {
             //
         })
     });
@@ -107,10 +112,11 @@ describe("PGN Viewer", function() {
         beforeEach(function() {
             clearHTML("b");
             var pgn = "1. e4 e5 2. Nf3 Nc6";
-            pgnv = pgnView("b", {pgn: pgn, position: "start"});
+            pgnView("b", {pgn: pgn, position: "start"});
+            pgnv = window.pgnTestRegistry['b'];
         });
 
-        it("should have an additional move", function() {
+        xit("should have an additional move", function() {
 
         })
     })
