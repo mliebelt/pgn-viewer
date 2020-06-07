bb8e840a1b5750fac11feacf7609636c118c9c29
Markus Liebelt <markusliebelt@gmail.com>
2020-06-07 15:18:26 +0200

    #153 Created an example, could not clearly reproduce the problem.
    Small fix to the webpack configuration in dev mode, so that examples can be used directly.

--- modules/pgn-viewer/webpack.dev.js
+++ modules/pgn-viewer/webpack.dev.js
@@ -5,7 +5,7 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './lib',
+        contentBase: '.',
         writeToDisk: true,
     },
 });
