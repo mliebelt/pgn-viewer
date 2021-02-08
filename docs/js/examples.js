45c62c8766f35c5b6f63f23670cc0ce9b0390415
Markus Liebelt <markusliebelt@gmail.com>
2021-02-08 07:42:34 +0100

    #189 Fixed missing brown theme in examples and ConfigBuilder.

--- docs/js/examples.js
+++ docs/js/examples.js
@@ -248,6 +248,19 @@ examples["1058"] = {
         pgnv = PGNV.pgnView("b1", {pgn: pgn, theme: 'blue'});
     }
 };
+examples["1059"] = {
+    desc: "The same game in a brown style",
+    html: "<div id=\"b1\" style=\"width: 360px\"><\/div>",
+    name: "Brown Style",
+    jsStr: "var pgn = ['[White \"Anderssen, Adolf\"][Black \"Dufresne, Jean\"]', \n '[Result \"1-0\"][ECO \"C52\"]', \n '[Site \"Berlin\"][Date \"1852.12.31\"]',\n '1. e4 e5 2. Nf3 Nc6  3.Bc4 Bc5 4.b4 Bxb4 5.c3 Ba5 6.d4 exd4 7.O-O d3 8.Qb3 Qf6 9.e5 Qg6 10.Re1 Nge7 11.Ba3 b5 12.Qxb5 Rb8 13.Qa4 Bb6 14.Nbd2 Bb7 15.Ne4 Qf5 16.Bxd3 Qh5 { now the whole idea unfolds } 17.Nf6+ gxf6 18.exf6 Rg8 19.Rad1!  Qxf3? 20.Rxe7+! Nxe7 21.Qxd7+ Kxd7 22.Bf5+ Ke8 23.Bd7+ Kf8 24. Bxe7# 1:0']\n.join(\" \");\npgnv = PGNV.pgnView('b1', {pgn: pgn, theme: 'brown'});",
+    jsFn: function() {
+        var pgn = ['[White "Anderssen, Adolf"][Black "Dufresne, Jean"]',
+            '[Result "1-0"][ECO "C52"]',
+            '[Site "Berlin"][Date "1852.12.31"]',
+            '1. e4 e5 2. Nf3 Nc6  3.Bc4 Bc5 4.b4 Bxb4 5.c3 Ba5 6.d4 exd4 7.O-O d3 8.Qb3 Qf6 9.e5 Qg6 10.Re1 Nge7 11.Ba3 b5 12.Qxb5 Rb8 13.Qa4 Bb6 14.Nbd2 Bb7 15.Ne4 Qf5 16.Bxd3 Qh5 { now the whole idea unfolds } 17.Nf6+ gxf6 18.exf6 Rg8 19.Rad1!  Qxf3? 20.Rxe7+! Nxe7 21.Qxd7+ Kxd7 22.Bf5+ Ke8 23.Bd7+ Kf8 24. Bxe7# 1:0'].join(" ");
+        pgnv = PGNV.pgnView("b1", {pgn: pgn, theme: 'brown'});
+    }
+};
 examples["1100"] = {
     desc: "This is an config of a chess game, where the moves are not only shown, but can be " +
         "changed. Try to play on the board, use the move, before and after comments, switch " +
